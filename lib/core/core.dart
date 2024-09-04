e431ac9620a79059e53b4317d656a9e18290c5a0
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2024-09-04 19:30:34 -0300

    creating usecases and typedef

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,3 +1,6 @@
 export 'common/common.dart';
+export 'errors/errors.dart';
 export 'res/res.dart';
 export 'services/services.dart';
+export 'usecases/usecases_imp.dart';
+export 'utils/utils.dart';
