b5ad6a62a24fcf5e7d462c9824c8f9d6cb798390
Helena Brito Dias <62960218+helenabd@users.noreply.github.com>
2024-09-04 19:30:11 -0300

    creating error files

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,36 @@
+import 'package:education_app/core/core.dart';
+
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({
+    required this.message,
+    required this.statusCode,
+  });
+
+  final String message;
+  final int statusCode;
+
+  String get errorMessage => '$statusCode' 'Error: $message';
+
+  @override
+  List<Object?> get props => [message, statusCode];
+}
+
+class CacheFailure extends Failure {
+  const CacheFailure({required super.message, required super.statusCode});
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({required super.message, required super.statusCode});
+
+  ServerFailure.fromException(ServerException exception)
+      : this(message: exception.message, statusCode: exception.statusCode);
+}
+
+class APIFailure extends Failure {
+  const APIFailure({required super.message, required super.statusCode});
+
+  APIFailure.fromException(APIException exception)
+      : this(message: exception.message, statusCode: exception.statusCode);
+}
