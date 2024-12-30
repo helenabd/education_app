0cc53b744cc77003bdc40d96e3416c0244fec906
Helena Brito Dias <helenabdias95@gmail.com>
2024-12-30 12:05:50 -0300

    creating auth repo implementation

---
+++ lib/src/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,70 @@
+import 'package:dartz/dartz.dart';
+import 'package:education_app/core/core.dart';
+import 'package:education_app/src/auth/data/data.dart';
+import 'package:education_app/src/auth/domain/domain.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  AuthRepoImpl(this._remoteDataSource);
+
+  final AuthRemoteDataSource _remoteDataSource;
+
+  @override
+  ResultFuture<void> forgotPassword(String email) async {
+    try {
+      await _remoteDataSource.forgotPassword(email);
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
+  }
+
+  @override
+  ResultFuture<LocalUser> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final result = await _remoteDataSource.signIn(
+        email: email,
+        password: password,
+      );
+
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
+  }
+
+  @override
+  ResultFuture<void> signUp({
+    required String email,
+    required String password,
+    required String fullName,
+  }) async {
+    try {
+      await _remoteDataSource.signUp(
+        email: email,
+        password: password,
+        fullName: fullName,
+      );
+
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
+  }
+
+  @override
+  ResultFuture<void> updateUser({
+    required UpdateUserAction action,
+    required dynamic userData,
+  }) async {
+    try {
+      await _remoteDataSource.updateUser(action: action, userData: userData);
+
+      return const Right(null);
+    } on ServerException catch (e) {
+      return Left(ServerFailure(message: e.message, statusCode: e.statusCode));
+    }
+  }
+}
--- lib/src/auth/domain/repos/auth_repo.dart
+++ lib/src/auth/domain/repos/auth_repo.dart
@@ -19,6 +19,6 @@ abstract class AuthRepo {
 
   ResultFuture<void> updateUser({
     required UpdateUserAction action,
-    dynamic userData,
+    required dynamic userData,
   });
 }
